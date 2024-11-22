b4145d3d50b858c7dc0c6f017f2b124166a49337
Krisna VJ <vjkrisna995@gmail.com>
2024-11-22 21:32:52 +0530

    Create valid2.py

---
+++ allFiles/inputFiles/valid2.py
@@ -0,0 +1,8 @@
+a = 5                                                                                                                                                                   
+b = a % 2                                                                                                                                                               
+if a == 0:                                                                                                                                                              
+    print("ZERO", )                                                                                                                                                     
+elif b == 0:                                                                                                                                                            
+    print("EVEN", )                                                                                                                                                     
+else:                                                                                                                                                                   
+    print("ODD", ) 
