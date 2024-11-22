dedbe87345f05f3a54793c4548de0e6dd9d47f95
Krisna VJ <vjkrisna995@gmail.com>
2024-11-22 22:45:57 +0530

    Create valid4.py

---
+++ allFiles/inputFiles/valid4.py
@@ -0,0 +1,11 @@
+# match(switch) construct
+
+a = 7                                                                                                                                                                   
+b = a % 2                                                                                                                                                               
+match b:                                                                                                                                                                
+    case 1:                                                                                                                                                             
+        print("Odd", )                                                                                                                                                  
+        break                                                                                                                                                           
+    case 0:                                                                                                                                                             
+        print("Even", )                                                                                                                                                 
+        break 
